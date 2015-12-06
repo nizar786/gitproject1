eed038be6eac9b4cd9826086c2bdf00ded16bc2c
nizarhussain <nizar_computer786@outlook.com>
2015-12-05 19:30:37 -0600

    modified project

---
+++ SpringBatchCSVAssignment/target/classes/dbscripts/assignment.sql
@@ -0,0 +1,336 @@
+CREATE DATABASE  IF NOT EXISTS `assignment` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `assignment`;
+-- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
+--
+-- Host: 127.0.0.1    Database: assignment
+-- ------------------------------------------------------
+-- Server version	5.6.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `BATCH_JOB_EXECUTION`
+--
+
+DROP TABLE IF EXISTS `BATCH_JOB_EXECUTION`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BATCH_JOB_EXECUTION` (
+  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
+  `VERSION` bigint(20) DEFAULT NULL,
+  `JOB_INSTANCE_ID` bigint(20) NOT NULL,
+  `CREATE_TIME` datetime NOT NULL,
+  `START_TIME` datetime DEFAULT NULL,
+  `END_TIME` datetime DEFAULT NULL,
+  `STATUS` varchar(10) DEFAULT NULL,
+  `EXIT_CODE` varchar(2500) DEFAULT NULL,
+  `EXIT_MESSAGE` varchar(2500) DEFAULT NULL,
+  `LAST_UPDATED` datetime DEFAULT NULL,
+  `JOB_CONFIGURATION_LOCATION` varchar(2500) DEFAULT NULL,
+  PRIMARY KEY (`JOB_EXECUTION_ID`),
+  KEY `JOB_INST_EXEC_FK` (`JOB_INSTANCE_ID`),
+  CONSTRAINT `JOB_INST_EXEC_FK` FOREIGN KEY (`JOB_INSTANCE_ID`) REFERENCES `BATCH_JOB_INSTANCE` (`JOB_INSTANCE_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BATCH_JOB_EXECUTION`
+--
+
+LOCK TABLES `BATCH_JOB_EXECUTION` WRITE;
+/*!40000 ALTER TABLE `BATCH_JOB_EXECUTION` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BATCH_JOB_EXECUTION` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BATCH_JOB_EXECUTION_CONTEXT`
+--
+
+DROP TABLE IF EXISTS `BATCH_JOB_EXECUTION_CONTEXT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BATCH_JOB_EXECUTION_CONTEXT` (
+  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
+  `SHORT_CONTEXT` varchar(2500) NOT NULL,
+  `SERIALIZED_CONTEXT` text,
+  PRIMARY KEY (`JOB_EXECUTION_ID`),
+  CONSTRAINT `JOB_EXEC_CTX_FK` FOREIGN KEY (`JOB_EXECUTION_ID`) REFERENCES `BATCH_JOB_EXECUTION` (`JOB_EXECUTION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BATCH_JOB_EXECUTION_CONTEXT`
+--
+
+LOCK TABLES `BATCH_JOB_EXECUTION_CONTEXT` WRITE;
+/*!40000 ALTER TABLE `BATCH_JOB_EXECUTION_CONTEXT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BATCH_JOB_EXECUTION_CONTEXT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BATCH_JOB_EXECUTION_PARAMS`
+--
+
+DROP TABLE IF EXISTS `BATCH_JOB_EXECUTION_PARAMS`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BATCH_JOB_EXECUTION_PARAMS` (
+  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
+  `TYPE_CD` varchar(6) NOT NULL,
+  `KEY_NAME` varchar(100) NOT NULL,
+  `STRING_VAL` varchar(250) DEFAULT NULL,
+  `DATE_VAL` datetime DEFAULT NULL,
+  `LONG_VAL` bigint(20) DEFAULT NULL,
+  `DOUBLE_VAL` double DEFAULT NULL,
+  `IDENTIFYING` char(1) NOT NULL,
+  KEY `JOB_EXEC_PARAMS_FK` (`JOB_EXECUTION_ID`),
+  CONSTRAINT `JOB_EXEC_PARAMS_FK` FOREIGN KEY (`JOB_EXECUTION_ID`) REFERENCES `BATCH_JOB_EXECUTION` (`JOB_EXECUTION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BATCH_JOB_EXECUTION_PARAMS`
+--
+
+LOCK TABLES `BATCH_JOB_EXECUTION_PARAMS` WRITE;
+/*!40000 ALTER TABLE `BATCH_JOB_EXECUTION_PARAMS` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BATCH_JOB_EXECUTION_PARAMS` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BATCH_JOB_EXECUTION_SEQ`
+--
+
+DROP TABLE IF EXISTS `BATCH_JOB_EXECUTION_SEQ`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BATCH_JOB_EXECUTION_SEQ` (
+  `ID` bigint(20) NOT NULL,
+  `UNIQUE_KEY` char(1) NOT NULL,
+  UNIQUE KEY `UNIQUE_KEY_UN` (`UNIQUE_KEY`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BATCH_JOB_EXECUTION_SEQ`
+--
+
+LOCK TABLES `BATCH_JOB_EXECUTION_SEQ` WRITE;
+/*!40000 ALTER TABLE `BATCH_JOB_EXECUTION_SEQ` DISABLE KEYS */;
+INSERT INTO `BATCH_JOB_EXECUTION_SEQ` VALUES (0,'0');
+/*!40000 ALTER TABLE `BATCH_JOB_EXECUTION_SEQ` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BATCH_JOB_INSTANCE`
+--
+
+DROP TABLE IF EXISTS `BATCH_JOB_INSTANCE`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BATCH_JOB_INSTANCE` (
+  `JOB_INSTANCE_ID` bigint(20) NOT NULL,
+  `VERSION` bigint(20) DEFAULT NULL,
+  `JOB_NAME` varchar(100) NOT NULL,
+  `JOB_KEY` varchar(32) NOT NULL,
+  PRIMARY KEY (`JOB_INSTANCE_ID`),
+  UNIQUE KEY `JOB_INST_UN` (`JOB_NAME`,`JOB_KEY`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BATCH_JOB_INSTANCE`
+--
+
+LOCK TABLES `BATCH_JOB_INSTANCE` WRITE;
+/*!40000 ALTER TABLE `BATCH_JOB_INSTANCE` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BATCH_JOB_INSTANCE` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BATCH_JOB_SEQ`
+--
+
+DROP TABLE IF EXISTS `BATCH_JOB_SEQ`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BATCH_JOB_SEQ` (
+  `ID` bigint(20) NOT NULL,
+  `UNIQUE_KEY` char(1) NOT NULL,
+  UNIQUE KEY `UNIQUE_KEY_UN` (`UNIQUE_KEY`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BATCH_JOB_SEQ`
+--
+
+LOCK TABLES `BATCH_JOB_SEQ` WRITE;
+/*!40000 ALTER TABLE `BATCH_JOB_SEQ` DISABLE KEYS */;
+INSERT INTO `BATCH_JOB_SEQ` VALUES (0,'0');
+/*!40000 ALTER TABLE `BATCH_JOB_SEQ` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BATCH_STEP_EXECUTION`
+--
+
+DROP TABLE IF EXISTS `BATCH_STEP_EXECUTION`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BATCH_STEP_EXECUTION` (
+  `STEP_EXECUTION_ID` bigint(20) NOT NULL,
+  `VERSION` bigint(20) NOT NULL,
+  `STEP_NAME` varchar(100) NOT NULL,
+  `JOB_EXECUTION_ID` bigint(20) NOT NULL,
+  `START_TIME` datetime NOT NULL,
+  `END_TIME` datetime DEFAULT NULL,
+  `STATUS` varchar(10) DEFAULT NULL,
+  `COMMIT_COUNT` bigint(20) DEFAULT NULL,
+  `READ_COUNT` bigint(20) DEFAULT NULL,
+  `FILTER_COUNT` bigint(20) DEFAULT NULL,
+  `WRITE_COUNT` bigint(20) DEFAULT NULL,
+  `READ_SKIP_COUNT` bigint(20) DEFAULT NULL,
+  `WRITE_SKIP_COUNT` bigint(20) DEFAULT NULL,
+  `PROCESS_SKIP_COUNT` bigint(20) DEFAULT NULL,
+  `ROLLBACK_COUNT` bigint(20) DEFAULT NULL,
+  `EXIT_CODE` varchar(2500) DEFAULT NULL,
+  `EXIT_MESSAGE` varchar(2500) DEFAULT NULL,
+  `LAST_UPDATED` datetime DEFAULT NULL,
+  PRIMARY KEY (`STEP_EXECUTION_ID`),
+  KEY `JOB_EXEC_STEP_FK` (`JOB_EXECUTION_ID`),
+  CONSTRAINT `JOB_EXEC_STEP_FK` FOREIGN KEY (`JOB_EXECUTION_ID`) REFERENCES `BATCH_JOB_EXECUTION` (`JOB_EXECUTION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BATCH_STEP_EXECUTION`
+--
+
+LOCK TABLES `BATCH_STEP_EXECUTION` WRITE;
+/*!40000 ALTER TABLE `BATCH_STEP_EXECUTION` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BATCH_STEP_EXECUTION` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BATCH_STEP_EXECUTION_CONTEXT`
+--
+
+DROP TABLE IF EXISTS `BATCH_STEP_EXECUTION_CONTEXT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BATCH_STEP_EXECUTION_CONTEXT` (
+  `STEP_EXECUTION_ID` bigint(20) NOT NULL,
+  `SHORT_CONTEXT` varchar(2500) NOT NULL,
+  `SERIALIZED_CONTEXT` text,
+  PRIMARY KEY (`STEP_EXECUTION_ID`),
+  CONSTRAINT `STEP_EXEC_CTX_FK` FOREIGN KEY (`STEP_EXECUTION_ID`) REFERENCES `BATCH_STEP_EXECUTION` (`STEP_EXECUTION_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BATCH_STEP_EXECUTION_CONTEXT`
+--
+
+LOCK TABLES `BATCH_STEP_EXECUTION_CONTEXT` WRITE;
+/*!40000 ALTER TABLE `BATCH_STEP_EXECUTION_CONTEXT` DISABLE KEYS */;
+/*!40000 ALTER TABLE `BATCH_STEP_EXECUTION_CONTEXT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `BATCH_STEP_EXECUTION_SEQ`
+--
+
+DROP TABLE IF EXISTS `BATCH_STEP_EXECUTION_SEQ`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `BATCH_STEP_EXECUTION_SEQ` (
+  `ID` bigint(20) NOT NULL,
+  `UNIQUE_KEY` char(1) NOT NULL,
+  UNIQUE KEY `UNIQUE_KEY_UN` (`UNIQUE_KEY`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BATCH_STEP_EXECUTION_SEQ`
+--
+
+LOCK TABLES `BATCH_STEP_EXECUTION_SEQ` WRITE;
+/*!40000 ALTER TABLE `BATCH_STEP_EXECUTION_SEQ` DISABLE KEYS */;
+INSERT INTO `BATCH_STEP_EXECUTION_SEQ` VALUES (0,'0');
+/*!40000 ALTER TABLE `BATCH_STEP_EXECUTION_SEQ` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `DATA_INPUT`
+--
+
+DROP TABLE IF EXISTS `DATA_INPUT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DATA_INPUT` (
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `visits` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DATA_INPUT`
+--
+
+LOCK TABLES `DATA_INPUT` WRITE;
+/*!40000 ALTER TABLE `DATA_INPUT` DISABLE KEYS */;
+INSERT INTO `DATA_INPUT` VALUES ('Mickey','Mouse','Main','1 Minnie','Florida',NULL),('Pluto','Dog','Side',NULL,NULL,NULL),('Donald','Duck','Side',NULL,NULL,2),('Minnie','Mouse','Side',NULL,NULL,NULL),('Minnie','Mouse','','25 Mickey','California',5),('Mickey','Mouse','','25 Mickey',NULL,NULL),('Donald','Duck',NULL,NULL,'Florida',NULL),('Pluto','Dog','','10 Pluto',NULL,NULL),('Donald','Duck',NULL,'20 Ducks',NULL,NULL),('Pluto','Dog',NULL,NULL,'California',1),('Mickey','Mouse',NULL,NULL,NULL,4);
+/*!40000 ALTER TABLE `DATA_INPUT` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `DATA_OUTPUT`
+--
+
+DROP TABLE IF EXISTS `DATA_OUTPUT`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `DATA_OUTPUT` (
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `visits` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `DATA_OUTPUT`
+--
+
+LOCK TABLES `DATA_OUTPUT` WRITE;
+/*!40000 ALTER TABLE `DATA_OUTPUT` DISABLE KEYS */;
+INSERT INTO `DATA_OUTPUT` VALUES ('Mickey','Mouse','Main','Address','Florida',4),('Pluto','Dog','Side','Address','California',1),('Donald','Duck','Side','Address','Florida',2),('Minnie','Mouse','Side','Address','California',5);
+/*!40000 ALTER TABLE `DATA_OUTPUT` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-11-01 21:48:05
